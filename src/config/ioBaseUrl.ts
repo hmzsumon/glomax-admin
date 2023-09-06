5f72eda81fb93202227296ef2c854e88094aa336
Zakaria <zaknetsum@gmail.com>
2023-09-07 00:22:26 +0600

    aws testin

--- src/config/ioBaseUrl.ts
+++ src/config/ioBaseUrl.ts
@@ -4,7 +4,7 @@ if (process.env.NODE_ENV === 'development') {
 	ioBaseUrl = 'http://localhost:5000';
 	console.log('Running in development mode');
 } else {
-	ioBaseUrl = 'http://16.171.227.53:5000';
+	ioBaseUrl = 'https://wfcmining.com';
 	// Code specific to production mode
 	console.log('Running in production mode');
 }
